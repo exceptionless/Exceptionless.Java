ccdfbb99372dba2e555b7cca54ef23982fe701ff
Prashant <prashantchaubey9795@gmail.com>
2021-04-07 08:13:58 +0100

    Small bug

--- src/main/java/com/exceptionless/exceptionlessclient/configuration/PluginManager.java
+++ src/main/java/com/exceptionless/exceptionlessclient/configuration/PluginManager.java
@@ -7,6 +7,7 @@ import com.exceptionless.exceptionlessclient.plugins.preconfigured.*;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.function.BiConsumer;
@@ -19,6 +20,7 @@ public class PluginManager {
   @Builder
   public PluginManager(LogIF log) {
     this.log = log;
+    this.plugins = new ArrayList<>();
     configureDefaultPlugins();
     sortPlugins();
   }
