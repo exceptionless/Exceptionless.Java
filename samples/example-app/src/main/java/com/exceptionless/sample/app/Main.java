8d8b756c2c62e2b84d877d72645900bc34b683f0
Prashant <prashantchaubey9795@gmail.com>
2021-04-17 19:43:15 +0100

    Updated example app

--- samples/example-app/src/main/java/com/exceptionless/sample/app/Main.java
+++ samples/example-app/src/main/java/com/exceptionless/sample/app/Main.java
@@ -1,24 +1,40 @@
 package com.exceptionless.sample.app;
 
 import com.exceptionless.exceptionlessclient.ExceptionlessClient;
+import com.exceptionless.exceptionlessclient.models.EventPluginContext;
 
 public class Main {
-  public static void main(String[] args) {
-    ExceptionlessClient client =
-        ExceptionlessClient.from(
-            System.getenv("EXCEPTIONLESS_SAMPLE_APP_API_KEY"),
-            System.getenv("EXCEPTIONLESS_SAMPLE_APP_SERVER_URL"));
+  private static final ExceptionlessClient client =
+      ExceptionlessClient.from(
+          System.getenv("EXCEPTIONLESS_SAMPLE_APP_API_KEY"),
+          System.getenv("EXCEPTIONLESS_SAMPLE_APP_SERVER_URL"));
 
-    client.getConfigurationManager().useSessions();
-
-    client.submitSessionStart();
-
+  public static void sampleEventSubmissions() {
     client.submitException(new RuntimeException("Test exception"));
-    client.submitUnhandledException(new RuntimeException("Test exception"),"Test submission method");
+    client.submitUnhandledException(
+        new RuntimeException("Test exception"), "Test submission method");
     client.submitFeatureUsage("Test feature");
     client.submitLog("Test log");
     client.submitNotFound("Test resource");
+  }
 
-    client.submitSessionEnd("Test user id");
+  public static void sampleUseOfSessions() {
+    client.getConfigurationManager().useSessions();
+    client.submitEvent(
+        EventPluginContext.from(client.createSessionStart().userIdentity("test-user").build()));
+    client.submitSessionEnd("test-user");
+  }
+
+  public static void sampleUseOfUpdatingEmailAndDescription() {
+    client.submitEvent(
+        EventPluginContext.from(
+            client.createLog("test-log").referenceId("test-reference-id").build()));
+    client.updateEmailAndDescription("test-reference-id", "test@email.com", "test-description");
+  }
+
+  public static void main(String[] args) {
+    sampleEventSubmissions();
+    sampleUseOfUpdatingEmailAndDescription();
+    sampleUseOfSessions();
   }
 }
